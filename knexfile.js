b0d8b702650dd57e8502c7974400db6b5420a83b
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-29 02:34:04 +0000

    initial table made

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+const settings = require('./settings');
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
